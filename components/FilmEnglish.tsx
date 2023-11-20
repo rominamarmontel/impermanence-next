c4acbe575638fe337a42fc8fbcc61fa5796a4f6c
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-11-20 11:19:34 +0100

    React.FC added in FilmEnglish

--- components/FilmEnglish.tsx
+++ components/FilmEnglish.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 import { useLanguage } from '@/app/LanguageContext'
 import React from 'react'
 
-const FilmEnglish = ({
+const FilmEnglish: React.FC<TFilm> = ({
   _id,
   category,
   title,
