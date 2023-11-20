06c661f5032240cac4d41e398cbb441993907e07
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-11-20 10:40:59 +0100

    display fixed FilmEnglish

--- components/FilmEnglish.tsx
+++ components/FilmEnglish.tsx
@@ -5,47 +5,52 @@ import styles from './styles.module.css'
 import { useLanguage } from '@/app/LanguageContext'
 import React from 'react'
 
-export const FilmEnglish = React.memo(
-  ({ _id, category, title, directedBy, imageData, originalTitle }: TFilm) => {
-    const { isEnglish } = useLanguage()
-    const getCategoryDisplayName = (catName: string) => {
-      return isEnglish && catName === 'en cours' ? 'in progress' : catName
-    }
-    return (
-      <div className="lg:w-[32%] md:w-[49%] sm:w-full">
-        <Link href={`${process.env.NEXTAUTH_URL}/en/film/${_id}`}>
-          <div style={{ position: 'relative' }} className="w-full aspect-video">
-            {imageData && imageData.length > 0 && (
-              <Image
-                src={imageData[0].url}
-                alt={originalTitle}
-                fill
-                sizes="(max-width: 640px) 100vw, (max-width: 768px) 80vw, 1200px"
-                className="object-cover object-center"
-                priority
-              />
-            )}
-          </div>
-
-          <div className="mt-4">
-            {title && (
-              <div className={styles.Title}>
-                {title.en && <div>{title.en.toUpperCase()}</div>}
-              </div>
-            )}
-            <div className={styles.DirectedBy}>
-              {directedBy && <div>by {directedBy}</div>}
-            </div>
-          </div>
-        </Link>
-        {category && (
-          <Link href={`categories/${getCategoryDisplayName(category.catName)}`}>
-            <span className={`${styles.CatName}`}>
-              {getCategoryDisplayName(category.catName).toUpperCase()}
-            </span>
-          </Link>
-        )}
-      </div>
-    )
+export const FilmEnglish = ({
+  _id,
+  category,
+  title,
+  directedBy,
+  imageData,
+  originalTitle,
+}: TFilm) => {
+  const { isEnglish } = useLanguage()
+  const getCategoryDisplayName = (catName: string) => {
+    return isEnglish && catName === 'en cours' ? 'in progress' : catName
   }
-)
+  return (
+    <div className="lg:w-[32%] md:w-[49%] sm:w-full">
+      <Link href={`${process.env.NEXTAUTH_URL}/en/film/${_id}`}>
+        <div style={{ position: 'relative' }} className="w-full aspect-video">
+          {imageData && imageData.length > 0 && (
+            <Image
+              src={imageData[0].url}
+              alt={originalTitle}
+              fill
+              sizes="(max-width: 640px) 100vw, (max-width: 768px) 80vw, 1200px"
+              className="object-cover object-center"
+              priority
+            />
+          )}
+        </div>
+
+        <div className="mt-4">
+          {title && (
+            <div className={styles.Title}>
+              {title.en && <div>{title.en.toUpperCase()}</div>}
+            </div>
+          )}
+          <div className={styles.DirectedBy}>
+            {directedBy && <div>by {directedBy}</div>}
+          </div>
+        </div>
+      </Link>
+      {category && (
+        <Link href={`categories/${getCategoryDisplayName(category.catName)}`}>
+          <span className={`${styles.CatName}`}>
+            {getCategoryDisplayName(category.catName).toUpperCase()}
+          </span>
+        </Link>
+      )}
+    </div>
+  )
+}
